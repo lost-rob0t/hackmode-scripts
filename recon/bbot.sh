35557bc6462fcc8d6d9a38aaf2f9d99e05de3c50
AlexDowneyJr <42532723+AlexDowneyJr@users.noreply.github.com>
2024-02-14 20:03:40 +0400

    Update bbot.sh
    Removing a redundant mkdir command

--- recon/bbot.sh
+++ recon/bbot.sh
@@ -8,7 +8,6 @@ function crawl () {
 	dir="$PWD/scans/$now/"
 	mkdir -p $PWD/bbot
 	mkdir $PWD/bbot/$now/
-	mkdir $PWD/bbot/$now
 	echo $dir
 	bbot --force -y -t $1 --blacklist blacklist.txt --name $1  -f subdomain-enum  cloud-enum web-basic   -m gowitness httpx   --output-module human csv json -o "$dir"
 }
